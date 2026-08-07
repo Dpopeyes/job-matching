da8470c57666028030ef7ebbf8a9b28d4afdaff9
Chayathon <pounzazakub@gmail.com>
2026-08-07 07:44:31 +0700

    Switch brand logo image file format to JPG (logo.jpg)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,19 +16,19 @@ export default function Navbar({ activeTab, setActiveTab, currentUser, setCurren
       <div className="app-container">
         <div className="navbar-inner">
           
-          {/* Logo & Project Title featuring Larger Pink Owl Mascot & Josefin Sans Font */}
+          {/* Logo & Project Title featuring JPG Pink Tech Suit Owl Mascot & Josefin Sans Font */}
           <div 
             style={{ display: 'flex', alignItems: 'center', gap: '10px', cursor: 'pointer' }}
             onClick={() => handleNavClick('home')}
           >
             <div className="brand-logo-box-mini">
               <img 
-                src="/logo.svg" 
-                alt="FreshGrad Jobs Pink Owl Logo" 
+                src="/logo.jpg" 
+                alt="FreshGrad Jobs Pink Owl Logo JPG" 
                 style={{ 
                   width: '42px', 
                   height: '42px', 
-                  objectFit: 'contain'
+                  objectFit: 'cover'
                 }}
               />
             </div>
